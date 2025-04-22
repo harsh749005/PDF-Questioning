ff26e45a0e524d7a622b994cfef5d39696b94c04
Harsh <patelharsh749005@gmail.com>
2025-04-22 11:10:08 +0530

    text converted to chunk

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 from PyPDF2 import PdfReader
 
-reader = PdfReader("shreenath.pdf")
-text = ""
+def extract_text(pdf_path):
+    reader = PdfReader(pdf_path)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text()
+    return text
 
-for page in reader.pages:
-    text += page.extract_text()
 
-print(text)
+
+def text_to_chunk(text ,chunk_size=500,overlap=50):
+    chunks = []
+    start = 0
+    while start < len(text):
+        end = start + chunk_size
+        chunks.append(text[start:end])
+        start += chunk_size - overlap
+    return chunks
+
+# Extract text
+result = extract_text("shreenath.pdf")
+print(result) 
+
+# Convert to chunk
+chunk = text_to_chunk(result)
+print(len(chunk))
