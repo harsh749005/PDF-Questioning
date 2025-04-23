efcc406e9e807cea520dc627b3f45bf2eb95a14d
Harsh <patelharsh749005@gmail.com>
2025-04-23 13:31:30 +0530

    vector is created

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from PyPDF2 import PdfReader
+from sentence_transformers import SentenceTransformer
 
 def extract_text(pdf_path):
     reader = PdfReader(pdf_path)
@@ -18,6 +19,14 @@ def text_to_chunk(text ,chunk_size=500,overlap=50):
         start += chunk_size - overlap
     return chunks
 
+
+model = SentenceTransformer('all-MiniLM-L6-v2')  # Light and fast
+
+def get_embeddings(chunks):
+    embeddings = model.encode(chunks)
+    return embeddings
+
+
 # Extract text
 result = extract_text("shreenath.pdf")
 print(result) 
@@ -25,3 +34,7 @@ print(result)
 # Convert to chunk
 chunk = text_to_chunk(result)
 print(len(chunk))
+
+#chunks to vector
+vector = get_embeddings(chunks=chunk)
+print(vector)
